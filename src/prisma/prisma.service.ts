46842caa0e86e9a0657f6e5e0ea066762e3849e3
Rohan216437 <7039426154rg@gmail.com>
2026-01-03 23:51:01 +0530

    prisma accelerator fixed

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,24 @@
-// src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
+  // constructor() {
+  //   const accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
+  //   if (accelerateUrl) {
+  //     super({ accelerateUrl });
+  //   }
+  // }
   constructor() {
     super({
-      accelerateUrl: process.env.PRISMA_ACCELERATE_URL,
-    });
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL!,
+      }),
+    })
   }
+  
   async onModuleInit() {
     await this.$connect();
   }
@@ -18,3 +27,26 @@ export class PrismaService extends PrismaClient
     await this.$disconnect();
   }
 }
+
+// import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+// import { PrismaClient } from "@prisma/client";
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   constructor() {
+//     super({
+//       accelerateUrl: process.env.PRISMA_ACCELERATE_URL,
+//     });
+//   }
+
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
